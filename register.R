1df8dd3b2a4b7279e60039754434295fd80629a0
Carl Boettiger <cboettig@gmail.com>
2020-02-21 19:17:08 +0000

    syntax?

--- register.R
+++ register.R
@@ -2,4 +2,4 @@
 urls <- paste0("https://github.com/boettiger-lab/data-tracker/raw/master/",
                list.files("data", full.names = TRUE, recursive = TRUE))
 
-contenturi::register(urls)
+lapply(urls, contenturi::register)
