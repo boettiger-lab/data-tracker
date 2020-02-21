c6c1ce2c7faaa5f2419a9a4e9dc34e96523bcd82
Carl Boettiger <cboettig@gmail.com>
2020-02-21 18:30:42 +0000

    fix syntax :construction:

--- data-tracker.R
+++ data-tracker.R
@@ -1,4 +1,9 @@
 dir.create("data/", FALSE)
 
-contenturi::store("ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_mm_mlo.txt", "data/")
+id <- contenturi::store("ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_mm_mlo.txt", "data/")
 
+
+path <- contenturi::retrieve(id, registries = "data/")
+path
+
+## We could get the GitHub data address and register that to remote registry
