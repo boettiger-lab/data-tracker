7f1cc248f7d8061ee9213630cb42dde080e2b3b0
Carl Boettiger <cboettig@gmail.com>
2020-02-22 01:37:50 +0000

    bagit example instead

--- data-tracker.R
+++ data-tracker.R
@@ -1,3 +1,3 @@
-dir.create("data/", FALSE)
+dir.create("store/", FALSE)
 
-contenturi::store("ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_mm_mlo.txt", "data/")
+contenturi::store("ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_mm_mlo.txt", "store/")
--- register.R
+++ register.R
@@ -1,5 +1,5 @@
 
 urls <- paste0("https://github.com/boettiger-lab/data-tracker/raw/master/",
-               list.files("data", full.names = TRUE, recursive = TRUE))
+               list.files("store", full.names = TRUE, recursive = TRUE))
 
-lapply(urls, contenturi::register, registries = c("https://hash-archive.org", "data/"))
+lapply(urls, contenturi::register, registries = c("https://hash-archive.org", "store/"))
