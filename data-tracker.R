121453ad824aa7cfa4e5e9d58b8b7080d21d78b6
Carl Boettiger <cboettig@gmail.com>
2020-03-05 06:17:58 +0000

    WIP

--- data-tracker.R
+++ data-tracker.R
@@ -1,3 +1,2 @@
-dir.create("store/", FALSE)
 
-contenturi::store("ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_mm_mlo.txt", "store/")
+contenturi::store("ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_weekly_mlo.txt", "store/")
