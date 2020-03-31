7e55216800dbc21054380fee92c85e1980fc013e
Carl Boettiger <cboettig@gmail.com>
2020-03-31 09:49:58 -0700

    Update data-tracker.R

--- data-tracker.R
+++ data-tracker.R
@@ -1,2 +1,2 @@
 
-contenturi::store("ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_weekly_mlo.txt", "store/")
+contentid::store("ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_weekly_mlo.txt", "store/")
